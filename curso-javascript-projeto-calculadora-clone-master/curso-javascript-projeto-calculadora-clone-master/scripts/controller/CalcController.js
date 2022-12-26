e0744e28b475344d3a6ebb7b18449ac789633af1
Ataize Feitosa <isa.aragao@hotmail.com>
2022-12-26 14:32:25 -0300

    Button events

--- curso-javascript-projeto-calculadora-clone-master/curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone-master/curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -9,6 +9,7 @@ class CalcController {
 
         this._currentDate;
         this.initialize();
+        this.initButtonEvents();
     }
 
     initialize(){
@@ -22,6 +23,30 @@ class CalcController {
            
         }, 1000);
 
+    }
+    //Vou criar um método que pegue tanto o click como o drop
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event =>{
+
+            element.addEventListener(event, fn, false);
+        })
+    }
+
+    initButtonEvents(){
+        //para pegar todas as tags g filhas de buttons e as tags g folhas de parts
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn ,'click drag', e =>{
+
+                console.log(btn.className.baseVal.replace("btn-", ""));
+
+            });
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e =>{
+                btn.style.cursor = 'pointer';
+            });
+        });
     }
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
